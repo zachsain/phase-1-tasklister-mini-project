73c58f10eafdd8f1261c41caafe453b4c49e6575
Zachary Sain <zachsain@gmail.com>
2021-09-28 12:56:53 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    
+  document.querySelector('form').addEventListener("submit", (e) => {
+  
+  e.preventDefault()
+  
+  addToDom(e.target.new_task.value)
+  
+})
+
 });
+
+function addToDom(todo){
+  
+   p = document.createElement('p')
+   p.textContent = todo
+   document.querySelector("#tasks").appendChild(p)
+  //  p.appendToParent
+  
+  
+}
+
+
+// create new element with p and append it to the parent 
+//
